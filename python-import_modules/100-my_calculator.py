2765c66f472a7059244d9d22467e7bc7e6434ec7
leen <leenalgraawi@gmail.com>
2026-06-03 21:50:40 +0300

    :1

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,10 +7,13 @@ def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     op = sys.argv[2]
+
     if op not in ["+", "-", "*", "/"]:
         print("Available operators: +, -, * and /")
         exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     c = {
@@ -19,8 +22,10 @@ def main():
      "*": mul,
      "/": div
     }
+
     print("{} {} {} = {}".format(a, op, b, c[op](a, b)))
 
+    exit(0)
 
 if __name__ == "__main__":
     main()
