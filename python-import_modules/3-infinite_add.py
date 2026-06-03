79f44845d937973b88c973b764ebabc1d62dbc5c
leen <leenalgraawi@gmail.com>
2026-06-03 16:28:32 +0300

    adding argss

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    k = 0
+    for i in range(1, len(sys.argv)):
+        k += int(sys.argv[i])
+    print(k)
+
+if __name__ == "__main__":
+    main()
