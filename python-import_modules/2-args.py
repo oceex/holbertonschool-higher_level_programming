c5134e9b56f34f7f9a497412f6ad8363f96a3f7d
leen <leenalgraawi@gmail.com>
2026-06-03 16:22:49 +0300

    argss

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+        return
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+
+
+
+if __name__ == "__main__":
+    main()
