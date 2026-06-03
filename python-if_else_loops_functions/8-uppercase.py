c80f97ddedc021d2503c3582369005329c05dbec
leen <leenalgraawi@gmail.com>
2026-06-03 12:55:05 +0300

    reio

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+islower = __import__('7-islower').islower
+def uppercase(str):
+    for c in str:
+        x = ord(c)
+        if islower(c):
+            x -= 32
+        print(chr(x), end="")
+
+    print()
