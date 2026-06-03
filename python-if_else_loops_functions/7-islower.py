08eccb1c2f0904b14d8b526a1a6c01781d7545ac
leen <leenalgraawi@gmail.com>
2026-06-03 12:39:25 +0300

    try catch

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if type(c) is not chr:
-        raise TypeError("s must be a char")
-    return c >= 'a' and c <= 'z'
+    try:
+        return c >= 'a' and c <= 'z'
+    except ValueError:
+        raise TypeError("wrong type")
