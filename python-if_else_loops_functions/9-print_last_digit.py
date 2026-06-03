8824b81199149a0be813e29bf799eb6db5787bd0
leen <leenalgraawi@gmail.com>
2026-06-03 13:04:18 +0300

    lst

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    num = number % 10 if number > 0 else number % -10
+    num = number % 10 if number > 0 else -(number % -10)
     print("{}".format(num), end="")
     return num
