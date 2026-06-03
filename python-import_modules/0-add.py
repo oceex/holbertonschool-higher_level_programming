a688134394a96cafe4db9a05bbddb385ba0d39af
leen <leenalgraawi@gmail.com>
2026-06-03 15:05:34 +0300

    main;>

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import add_0 as adding
+import add_0
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, adding.add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 if __name__ == "__main__":
     main()
