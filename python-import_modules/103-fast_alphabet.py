29617ef5b8dc177eefd1bbb50a0f4e3fce5705c4
leen <165122715+oceex@users.noreply.github.com>
2026-06-05 20:23:27 +0300

    Fix open function call in fast_alphabet.py

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-wow = lambda a=90: a > 65 and (wow(a - 1), open(1, "w").write(chr(i)))[1]
+wow = lambda a=90: a > 65 and (wow(a - 1), open(1, (str())[0:0]).write(chr(i)))[1]
 wow() or print()
