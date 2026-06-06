dcd2d22ef5b8bb423d99b1874952f67b9de16950
leen <leenalgraawi@gmail.com>
2026-06-06 06:46:24 +0300

    what a magic

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -3,7 +3,6 @@
 from magic_calculation_102 import add, sub
 
 
-
 def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
@@ -12,4 +11,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-
