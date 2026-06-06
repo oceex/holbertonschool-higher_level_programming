c3c6efab1a890a9d8b8cf14b36f67a768753f775
leen <165122715+oceex@users.noreply.github.com>
2026-06-06 07:11:36 +0300

    Refactor fast_alphabet to use list comprehension

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-wow = lambda a=90: a >= 65 and (wow(a - 1), print(chr(a), end=(str())[0:0]))[1]
-wow() or print()
+[print(chr(a), end=(str())[0:0]) for a in range(65, 91)]
+print()
