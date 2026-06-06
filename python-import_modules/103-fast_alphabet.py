ecc761237bd2b258735938d87d873afd7609bd1d
leen <165122715+oceex@users.noreply.github.com>
2026-06-06 07:07:59 +0300

    Refactor wow function to use lambda syntax

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def wow a=90: return a >= 65 and (wow(a-1), print(chr(a), end=(str())[0:0]))[1]
+wow = lambda a=90: a >= 65 and (wow(a - 1), print(chr(a), end=(str())[0:0]))[1]
 wow() or print()
