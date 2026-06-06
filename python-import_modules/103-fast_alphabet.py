dd51774c1af9554ddfec93c957ce939110d87a3b
leen <leenalgraawi@gmail.com>
2026-06-06 06:49:41 +0300

    i loved that one

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-wow = lambda a=90: a >= 65 and (wow(a - 1),print(chr(a), end=(str())[0:0]))[1]
+wow = lambda a=90: a >= 65 and (wow(a - 1), print(chr(a), end=(str())[0:0]))[1]
 wow() or print()
