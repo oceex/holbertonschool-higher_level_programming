edce8c710ab75288869f2a283b2771d2d9c95925
leen <leenalgraawi@gmail.com>
2026-06-06 06:53:49 +0300

    i love exce

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-exec("wow = lam" + "bda a=90: a >= 65 and (wow(a - 1), print(chr(a), end=(str())[0:0]))[1]")
+exec(str())[0:0] wow = lambda a=90: a >= 65 and (wow(a - 1), print(chr(a), end=(str())[0:0]))[1] str())[0:0])
 wow() or print()
