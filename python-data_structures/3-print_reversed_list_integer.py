b03abfb938d113212a310d353ed0af6c36ce7d25
leen <leenalgraawi@gmail.com>
2026-06-07 03:53:09 +0300

    print revesed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,5 @@
 print_list_integer = __import__('0-print_list_integer').print_list_integer
 def print_reversed_list_integer(my_list=[]):
     my_list = my_list[::-1]
-    print_list_integer(my_list)
+    for n in my_list:
+        print("{:d}".format(n))
