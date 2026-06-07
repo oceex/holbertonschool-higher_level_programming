53145cded05c676261d0c10f790679784c87e62f
leen <leenalgraawi@gmail.com>
2026-06-07 05:34:53 +0300

    new idea

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,6 @@
 
 def divisible_by_2(my_list=[]):
     k = []
-    for i in range(len(my_list)):
-        k = my_list[i] % 2 == 0
+    for n in my_list:
+        k.append(my_list[i] % 2 == 0)
     return k
