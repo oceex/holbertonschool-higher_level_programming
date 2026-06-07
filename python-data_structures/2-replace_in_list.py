0e931084c258fdb5394ec0cc47a252bfb15a1452
leen <leenalgraawi@gmail.com>
2026-06-07 03:47:43 +0300

    replacing

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+element_at = __import__('1-element_at').element_at
+def replace_in_list(my_list, idx, element):
+    if element_at(my_list, idx):
+       my_list[idx] = element
+    return my_list
