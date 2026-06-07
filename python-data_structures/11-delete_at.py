e98d2d5896b1c0ea0ca1130491e9a2c28e31883d
leen <leenalgraawi@gmail.com>
2026-06-07 05:40:15 +0300

    deleting

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,4 +5,4 @@ element_at = __import__('1-element_at').element_at
 def delete_at(my_list=[], idx=0):
     if element_at(my_list, idx):
         my_list.remove(my_list[idx])
-    return my_listy
+    return my_list
