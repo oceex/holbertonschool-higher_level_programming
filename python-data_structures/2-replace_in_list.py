5e75e7073354d1cd198f8eda5e2d815be49d411c
leen <leenalgraawi@gmail.com>
2026-06-07 03:48:45 +0300

    pyStyle

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 element_at = __import__('1-element_at').element_at
+
+
 def replace_in_list(my_list, idx, element):
     if element_at(my_list, idx):
-       my_list[idx] = element
+        my_list[idx] = element
     return my_list
