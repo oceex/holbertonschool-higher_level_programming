fff504f925f6620a7874d7329391b91a7c316710
leen <leenalgraawi@gmail.com>
2026-06-10 22:29:27 +0300

    simple sultion

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 
 
 def roman_to_int(roman_string):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    most = "I"
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'a': 0}
+    most = "a"
     for n in roman_string:
         if roman[n] > roman[most[0]]:
             most = n
