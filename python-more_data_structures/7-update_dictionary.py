99e3b0d8cc69becc0b9591315b76a1790579066b
leen <leenalgraawi@gmail.com>
2026-06-10 18:43:52 +0300

    dic

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+
+def update_dictionary(a_dictionary, key, value):
+    if not a_dictionary.get(key):
+        a_dictionary.update({key: value})
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
