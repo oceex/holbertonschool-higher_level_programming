390228152fdc4076fb620ea9f244f383b724e753
leen <leenalgraawi@gmail.com>
2026-06-10 19:28:49 +0300

    best

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        best = next(iter(a_dictionary.values()))
+    else:
+        return None
+    for n in a_dictionary:
+        if a_dictionary[n] > best:
+            best = a_dictionary[n]
