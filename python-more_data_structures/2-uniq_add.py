2f121a80fb3dfdef906c2ba15b354bd5141817d8
leen <leenalgraawi@gmail.com>
2026-06-10 17:48:29 +0300

    counting and removing

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,8 @@
 def uniq_add(my_list=[]):
     m = 0
     for n in my_list:
-        if my_list.count(n) == 1:
-            m += n
+        if my_list.count(n) > 1:
+            my_list.remove(n)
+    for n in my_list:
+        m += n
     return m
