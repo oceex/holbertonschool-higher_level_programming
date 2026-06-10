bcca68fa1833ce31dc23b7e786ee2241585b0235
leen <leenalgraawi@gmail.com>
2026-06-10 22:24:13 +0300

    ymy roman

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    most = "I"
+    for n in roman_string:
+        if roman[n] > roman[most[0]]:
+            most = n
+        elif roman[n] == roman[most[0]]:
+            most += n
+    s = roman_string.partition(most)
+    boss = 0
+    for i in range(len(s)):
+        for n in s[i]:
+           if i == 0:
+               boss -= roman[n]
+           else:
+               boss += roman[n]
+    return boss
