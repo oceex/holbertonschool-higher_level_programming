b968be463b67a502dd6ad245ed5a82173744307d
leen <leenalgraawi@gmail.com>
2026-06-10 23:05:45 +0300

    wow

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,11 +7,14 @@ def roman_to_int(roman_string):
 
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'a': 0}
     most = "a"
-    for n in roman_string:
-        if roman[n] > roman[most[0]]:
-            most = n
-        elif roman[n] == roman[most[0]]:
-            most += n
+    index = 0
+    for i in range(len(roman_string)):
+        if roman[roman_string[i]] > roman[most[0]]:
+            most = roman_string[i]
+            index = i
+        elif roman[roman_string[i]] == roman[most[0]] and i - 1 == index:
+            most += roman_string[i]
+
     s = roman_string.partition(most)
     boss = 0
     for n in most:
