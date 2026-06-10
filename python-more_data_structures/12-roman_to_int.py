d157eac7da112ff62305e21cefffd86a83553b96
leen <leenalgraawi@gmail.com>
2026-06-10 23:13:23 +0300

    wow

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,16 @@
 def roman_to_int(roman_string):
     if not type(roman_string) == type("leen"):
         return 0
-
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'a': 0}
+    roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            'a': 0
+            }
     most = "a"
     index = 0
     for i in range(len(roman_string)):
@@ -14,18 +22,15 @@ def roman_to_int(roman_string):
             index = i
         elif roman[roman_string[i]] == roman[most[0]] and i - 1 == index:
             most += roman_string[i]
-
     s = roman_string.partition(most)
     boss = 0
     for n in most:
         boss += roman[n]
-
     if s[0] == '' and s[2] == '':
         return boss
-
     for i in range(len(s)):
-       if i == 0:
+        if i == 0:
             boss -= roman_to_int(s[i])
-       elif i == 2:
+        elif i == 2:
             boss += roman_to_int(s[i])
     return boss
