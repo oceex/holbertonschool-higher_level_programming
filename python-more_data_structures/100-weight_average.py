8a32408ee2810af9f0b63ce651ab6c0ccd322349
leen <leenalgraawi@gmail.com>
2026-06-10 23:23:21 +0300

    free points

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    result = 0
+    for i in range(len(my_list)):
+        mul = 1
+        for j in range(len(my_list[i])):
+            mul *= my_list[i][j]
+        result += mul
+    return result
