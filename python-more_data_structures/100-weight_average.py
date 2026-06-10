354c5bc83b6e8d404de39ab621d41483822beb21
leen <leenalgraawi@gmail.com>
2026-06-10 23:32:22 +0300

    free points

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,10 +2,12 @@
 
 
 def weight_average(my_list=[]):
-    result = 0
+    result, k = 0, 0
     for i in range(len(my_list)):
         mul = 1
         for j in range(len(my_list[i])):
             mul *= my_list[i][j]
+            if j == 1:
+                k += my_list[i][j]
         result += mul
-    return result/len(my_list)
+    return result/k
