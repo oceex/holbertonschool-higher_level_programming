c29d6410956167f9af3ef794ef2276c0a0977958
leen <leenalgraawi@gmail.com>
2026-06-11 22:53:30 +0300

    pop

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    c = 1
+    while c:
+        c = 0
+        for n in a_dictionary:
+            if a_dictionary[n] == value:
+                a_dictionary.pop(n)
+                c = 1
