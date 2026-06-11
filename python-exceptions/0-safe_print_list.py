bdf33458b2ebed5c1ba7c8a3ad87b0e8d955d306
leen <leenalgraawi@gmail.com>
2026-06-12 01:11:04 +0300

    exceptionss

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             c += 1
-        print()
     except IndexError:
         pass
     finally:
+        print()
         return c
