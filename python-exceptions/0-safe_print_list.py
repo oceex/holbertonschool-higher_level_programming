fc388fb6cdf4665ef294752c5ac342347cab221a
leen <leenalgraawi@gmail.com>
2026-06-12 01:07:52 +0300

    exceptionss

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@
 def safe_print_list(my_list=[], x=0):
     c = 0
     try:
-        for n in my_list:
-            print(f"{n}", end="")
+        for n in range(x):
+            print(f"{my_list[n]}", end="")
             c += 1
         print()
     except IndexError:
