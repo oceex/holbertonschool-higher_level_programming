c37b8971e9331536de38cfec96d77a48cde72b09
leen <leenalgraawi@gmail.com>
2026-06-12 02:02:25 +0300

    while

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,14 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
             c += 1
             x -= 1
-        except Exception:
+        except (ValueError, TypeError):
             i += 1
             x -= 1
-        except IndexError:
-            print("IndexError: list index out of range")
-            break
-        except TypeError:
-            print("TypeError: list indices must be integers or slices, not str")
-            break
     print()
     return c
