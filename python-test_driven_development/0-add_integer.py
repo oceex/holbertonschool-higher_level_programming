b502dc62d02c43eed023d851e5675b8bc43b81db
leen <leenalgraawi@gmail.com>
2026-06-12 04:44:16 +0300

    what

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ def add_integer(a, b=98):
     """ Saifly Addition function """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a, b = int(a), int(b)
     return a + b
