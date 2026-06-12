87df68fbe1769a0311100cb6eda6340630ed5315
leen <leenalgraawi@gmail.com>
2026-06-12 03:14:17 +0300

    neww

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
     while not list_length == x:
         try:
             tmp = 0
-            tmp = my_list_1[x]/my_list_2[x]
+            tmp = my_list_1[x] / my_list_2[x]
 
         except ZeroDivisionError:
             print("division by 0")
