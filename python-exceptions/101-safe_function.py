76762b5a8cbd3ee026fc3977b45a4c88a57f06c2
leen <leenalgraawi@gmail.com>
2026-06-12 04:06:41 +0300

    space

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as x:
-        print("Exception: ", x.with_traceback(None), file=sys.stderr)
+        print("Exception:", x.with_traceback(None), file=sys.stderr)
         return None
