c48b67fcbf6673e4e2fed9d6383abe66050a6d7a
leen <leenalgraawi@gmail.com>
2026-06-12 04:43:41 +0300

    what

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ the sum of two numbers as an integer.
 
 def add_integer(a, b=98):
     """ Saifly Addition function """
-    if isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
