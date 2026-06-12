1d5ac2eb247af920a07fc6ea9411ff6689e807c7
leen <leenalgraawi@gmail.com>
2026-06-12 18:18:34 +0300

    square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+"""
+hi
+print square
+like
+
+##
+##
+
+"""
+
+
+def print_square(size):
+    """ printing square using # saifly """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in size:
+        for j in size:
+            print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
