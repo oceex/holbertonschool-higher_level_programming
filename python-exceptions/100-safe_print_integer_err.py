8bb75aace52a283ff42f532a38b5e03857df3443
leen <leenalgraawi@gmail.com>
2026-06-12 03:45:05 +0300

    traaace

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+import sys
 
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as e:
-        print("Exception:", e.with_traceback(None))
+        print("Exception:", e.with_traceback(None), file=sys.stderr)
         return False
     else:
         return True
