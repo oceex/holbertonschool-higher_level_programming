e04f4eef6ec665d0e8d765068dc3b610ad957b44
leen <leenalgraawi@gmail.com>
2026-06-12 18:20:16 +0300

    square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -18,8 +18,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in size:
-        for j in size:
+    for i in range(size):
+        for j in range(size):
             print("#", end="")
         print()
 
