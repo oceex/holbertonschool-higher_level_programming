443013e5a022f3eb77c7cbe1ee8dfe36d9f28f49
leen <leenalgraawi@gmail.com>
2026-06-14 18:10:06 +0300

    class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Module that defines a Square class."""
+""" Module that defines a Square class. """
 
 class Square:
-    """Represents an empty class."""
+    """ Represents an empty class. """
     pass
