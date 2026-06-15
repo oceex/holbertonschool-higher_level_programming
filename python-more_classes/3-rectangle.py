7c8aebef098919ac66d9ce45e3b142e79d7fa6ab
leen <leenalgraawi@gmail.com>
2026-06-15 19:06:28 +0300

    str

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -57,6 +57,6 @@ class Rectangle:
         for a in range(self.height):
             for b in range(self.width):
                 v += '#'
-            if a != self.height - 1
+            if not a == self.height - 1
                 v += '\n'
         return v
