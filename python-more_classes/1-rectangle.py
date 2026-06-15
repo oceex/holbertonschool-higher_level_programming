40e08980d78eb38adbc8b6bd37010a4e0175f260
leen <leenalgraawi@gmail.com>
2026-06-15 18:28:30 +0300

    recrec

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """ Rectangle blueprint """
     def __init__(self, width=0, height=0):
         " initoalasing new rectangle "
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
     @property
     def width(self):
         " get width "
