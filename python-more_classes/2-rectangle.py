65022b23a5c1a7f8e82ab638f7492e9a914eda6c
leen <leenalgraawi@gmail.com>
2026-06-15 18:50:39 +0300

    area per

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.__width = value
 
     @height.setter
     def height(self, value):
@@ -39,7 +39,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
 
     def area(self):
         " calculating the Rectangle's area "
