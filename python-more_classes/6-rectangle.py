9aa5cfcb22621144cafeab5f3feda0391eeab097
leen <leenalgraawi@gmail.com>
2026-06-16 16:31:20 +0300

    counting rec's

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+
+this mudule creats Rectangle
+
+"""
+
+
+class Rectangle:
+    " Rectanlge blueprint "
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        " initoalazing new Rec "
+        number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        " get width "
+        return self.__width
+
+    @property
+    def height(self):
+        " get height "
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        " setting width "
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        " setting height "
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        " calculating the Rectangle's area "
+        return self.height * self.width
+
+    def perimeter(self):
+        " calculating the Rectangle's per "
+        if self.height and self.width:
+            return 2 * (self.height + self.width)
+        return 0
+
+    def __str__(self):
+        " printing the cute little REC "
+        c = ""
+        for n in range(self.__height):
+            for m in range(self.__width):
+                c += '#'
+            if n != self.__height - 1 and self.width:
+                c += '\n'
+        if not self.__height:
+            c += '\n'
+        return c
+
+    def __repr__(self):
+        " to dublecate the rectangle "
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        " To say Goodbye to my little Rec:( "
+        number_of_instances -= 1
+        print("Bye rectangle...")
