ae8fa84c44b720f4b96bf8833bb3ba7de8e09b1d
leen <leenalgraawi@gmail.com>
2026-06-16 17:28:39 +0300

    comparing

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,6 +78,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         " compare between two rect's "
         if not isinstance(rect_1, Rectangle):
