4101e14bd28bcf2e144b0f7a58353a489de2ccee
leen <leenalgraawi@gmail.com>
2026-06-21 19:51:16 +0300

    0-3 :<

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+improve
+"""
+
+
+class BaseGeometry:
+    "the baasee "
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        " int "
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer",)
+        if value < 0:
+            raise ValueError(f"{name} must be greater than 0")
