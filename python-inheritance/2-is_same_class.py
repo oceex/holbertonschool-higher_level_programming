e6ea2e853049391d0f31bf567b137d302811bb9c
leen <leenalgraawi@gmail.com>
2026-06-21 19:16:21 +0300

    KSAvsESP

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -10,6 +10,4 @@ def is_same_class(obj, a_class):
         return False
     if type(obj) == int and a_class == bool:
         return False
-    if type == type(a_class):
-        return isinstance(obj, a_class)
     return isinstance(obj, type(a_class))
