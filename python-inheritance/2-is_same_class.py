0597901003a11f1158721d4e7db131471283851e
leen <leenalgraawi@gmail.com>
2026-06-21 19:21:51 +0300

    KSAvsESP

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,14 +6,4 @@ isn't?
 
 def is_same_class(obj, a_class):
     " are we? "
-    if type(obj) == bool and a_class == int:
-        return False
-    if type(obj) == int and a_class == bool:
-        return False
-    if type(obj) == bool and a_class == bool:
-        return True
-    if type(obj) == int and a_class == int:
-        return True
-    if type(obj) == list and a_class == list:
-        return True
-    return isinstance(obj, type(a_class))
+    return obj.__class__ == a_class
