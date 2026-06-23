60b6dca788c9f8482e3a516ba8ac2e584b68bfb3
leen <leenalgraawi@gmail.com>
2026-06-23 19:15:02 +0300

    k

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+abc
+"""
+from abc import ABC
+
+
+class Animal(ABC):
+    " abc class "
+    @abstractmethod
+    def sound(self):
+        " an abstract method "
+        pass
+
+class Cat(Animal):
+    " caca "
+    def sound(self):
+        " implementing method "
+        return "Meow"
+
+class Dog(Animal):
+    " dada "
+    def sound(self):
+        " implementing method "
+        return "Bark"
+
