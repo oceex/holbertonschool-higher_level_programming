9023a299d75b8d8ab9ee69ace8cb823bc9674c3d
leen <leenalgraawi@gmail.com>
2026-06-23 16:54:09 +0300

    k

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+SQUARE ;>
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class square(Rectangle):
+    " Square Square "
+    def __init__(self, size):
+        " intioalzing "
+        try:
+            integer_validator("size", size)
+        except Exception:
+            pass
+        else:
+            self.__size = size
+
+    def area(self):
+        " calculating the area "
+        return self.__size * self.__size
