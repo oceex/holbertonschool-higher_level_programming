ef9ca99d64f8ae0edf09cd6640eb0c8e40ccba99
leen <leenalgraawi@gmail.com>
2026-06-29 05:13:13 +0300

    k

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,12 +7,14 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 import json
 import sys
 
+def main():
+    " maimai "
+    x = load_from_json_file("add_item.json")
 
-x = load_from_json_file("add_item.json")
+    for n in sys.args:
+        x.append(n)
 
-for n in sys.args:
-    x.append(n)
+    save_to_json_file(x, "add_item.json")
 
-save_to_json_file(x, "add_item.json")
-
-
+if __name__ == ("__main__"):
+    main()
