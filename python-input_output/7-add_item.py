f6c4b28888a493e34a12e5debf6315f5c7f7d2a4
leen <165122715+oceex@users.noreply.github.com>
2026-06-29 18:09:44 +0300

    Refactor add_item.py to handle JSON file existence
    Refactor to check for existing JSON file before loading data. Update argument handling to use sys.argv.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,15 +6,18 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 import json
 import sys
+import os
+
 
 def main():
     " maimai "
-    x = load_from_json_file("add_item.json")
-
-    for n in sys.args:
+    x = []
+    if os.path.exists("add_item.json") and os.path.getsize("add_item.json"):
+        x = list(load_from_json_file("add_item.json"))
+    for n in sys.argv[1:]:
         x.append(n)
-
     save_to_json_file(x, "add_item.json")
 
-if __name__ == ("__main__"):
+
+if __name__ == "__main__":
     main()
