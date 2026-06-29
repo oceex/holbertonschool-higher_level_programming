ac74250663cf393b5b25c4fa30500cd4e1b45a6e
leen <leenalgraawi@gmail.com>
2026-06-29 03:27:04 +0300

    k

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+k kkk kk
+"""
+
+
+def append_write(filename="", text=""):
+    " appendinggg "
+    with open(filename, "a") as k:
+        n = k.write(text)
+    return n
