5a5ffe0d4f4a1091f57448f1c6cd346b270835a1
leen <leenalgraawi@gmail.com>
2026-06-29 05:06:06 +0300

    k

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,9 @@ k kkk k k k k kkk k k kk k
 
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
-import json, sys
+import json
+import sys
+
 
 x = load_from_json_file("add_item.json")
 
