4bd468d666ebec2c9a7cab059535e251efb8399b
leen <leenalgraawi@gmail.com>
2026-06-29 22:46:32 +0300

    k

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -6,20 +6,11 @@ k k kkk kk kk
 
 def append_after(filename="", search_string="", new_string=""):
     " k kkk k k k "
-    if new_string[-1:] == '\n':
-        new_string = new_string[:-1]
-    with open(filename, "r") as x:
-        v = x.read()
-    x = v.split('\n')
-    c = 0
-    while c < len(x):
-        for n in range(c, len(x)):
-            if search_string in x[n]:
-                x.insert(n+1, new_string)
-                c = n + 2
-                break
-            else:
-                c += 1
-    m = '\n'.join(x)
-    with open(filename, "w") as z:
-        z.write(m)
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
+            if new_string in line:
+                f.write(new_string)xy
