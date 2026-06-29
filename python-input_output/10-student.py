2bda53896497be4bf97c7c160727f01cb027f748
leen <leenalgraawi@gmail.com>
2026-06-29 19:00:43 +0300

    k

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+kk kk k k k
+"""
+
+
+class Student:
+    " kk k kk "
+    def __init__(self, first_name, last_name, age):
+        " kk k k kkk k "
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        " JSON "
+        if attrs == None or not all(type(x) == str for x in attrs):
+            return self.__dict__
+        else:
+            c = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    c[a] = self.__dict__[a]
+            return c
