b02b76b65dc708b50722e574b9da90c7f33790b3
leen <leenalgraawi@gmail.com>
2026-06-29 04:48:46 +0300

    k

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+kk kk k
+"""
+
+import json
+
+
+
+def save_to_json_file(my_obj, filename):
+    " reall jsooonn "
+    with open(filename, "w") as x:
+        json.dump(my_obj, filename)
