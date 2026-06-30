59a08f19ed813b3c3f802d2cf63433a3b4af1159
leen <leenalgraawi@gmail.com>
2026-06-30 18:34:00 +0300

    k

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+kk kkk k k k k kk
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    " saving to a json "
+    with open(filename, "w") as x:
+        json.dump(data, x)
+
+def load_and_deserialize(filename):
+    " loading from a json "
+    with open(filename, "r") as x:
+    return json.load(x)
