79a4a241cd64a40d302837275d9af9dc2a26447c
leen <leenalgraawi@gmail.com>
2026-07-19 22:07:16 +0300

    k

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,8 @@
 -- Convert database, table, and field to UTF8 (utf8mb4, utf8mb4_unicode_ci)
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+USE hbtn_0c_0;
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
