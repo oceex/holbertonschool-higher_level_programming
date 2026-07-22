79a5c92c544226a22cdf8bed93c00440c6f66c9b
leen <leenalgraawi@gmail.com>
2026-07-22 16:49:09 +0300

    k

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creating user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
