85591ba56babcd5ed4bdd0fc31ef849df8be56cb
leen <leenalgraawi@gmail.com>
2026-07-22 23:57:36 +0300

    k

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT title, name
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON g.id = sg.genre_id
+ORDER BY title, name;
