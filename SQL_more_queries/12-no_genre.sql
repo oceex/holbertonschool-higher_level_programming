e1a8f9e5af1b2615330ba3324270a83a044241fb
leen <leenalgraawi@gmail.com>
2026-07-22 20:36:02 +0300

    k

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 --  lists all shows contained in hbtn_0d_tvshows without a genre linked.
-USE hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
