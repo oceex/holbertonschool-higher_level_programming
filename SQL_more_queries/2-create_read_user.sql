300293f14e99114dc52fdc7a1d082ecdc5f7c8ed
leen <leenalgraawi@gmail.com>
2026-07-22 17:02:37 +0300

    k

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates database and use with select pilgoy on it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'host';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
