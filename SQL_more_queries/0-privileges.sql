ce74acc714ff30fade3404bead454349a6f19e42
leen <leenalgraawi@gmail.com>
2026-07-22 16:41:56 +0300

    k

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---show grantsss
-SHOW GRANTS FOR 'user_0d_1'@'localhost' \G
-SHOW GRANTS FOR 'user_0d_2'@'localhost' \G
+-- Lists all privileges of users user_0d_1 and user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
