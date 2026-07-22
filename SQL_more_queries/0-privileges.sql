f0834bcb5e965824eebce3dd2d85066bf49b22bc
leen <leenalgraawi@gmail.com>
2026-07-22 16:34:09 +0300

    k

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 --show grantsss
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT grantee, privilege_type, table_schema, table_name
+FROM information_schema.user_privileges
+WHERE grantee IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'");
