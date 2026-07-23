a6e09a578501e383bb6bc98035e4820ff2504b7b
leen <leenalgraawi@gmail.com>
2026-07-23 05:03:00 +0300

    k

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,10 +2,10 @@
 SELECT name
 FROM tv_genres
 WHERE name NOT IN (
-    SELECT name
-    FROM tv_genres g
-    JOIN tv_show_genres sg ON sg.genre_id = g.id
-    JOIN tv_shows s ON sg.show_id = s.id
-    WHERE s.title = 'Dexter'
-        )
+SELECT name
+FROM tv_genres g
+JOIN tv_show_genres sg ON sg.genre_id = g.id
+JOIN tv_shows s ON sg.show_id = s.id
+WHERE s.title = 'Dexter'
+)
 ORDER BY name;
