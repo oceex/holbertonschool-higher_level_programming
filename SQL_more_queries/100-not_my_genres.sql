a2ee8d0a3e3df7079590c8de156ac4af6d00537e
leen <leenalgraawi@gmail.com>
2026-07-23 04:40:55 +0300

    k

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -6,6 +6,6 @@ WHERE name NOT IN (
     FROM tv_genres g
     JOIN tv_show_genres sg ON sg.genre_id = g.id
     JOIN tv_shows s ON sg.show_id = s.id
-    WHERE s.title = 'New Girl'
+    WHERE s.title = 'Dexter'
         )
 ORDER BY name;
