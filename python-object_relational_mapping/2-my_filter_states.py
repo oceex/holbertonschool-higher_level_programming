d72210fee43be564e8cf8b70fb247564fb0a2ecb
leen <leenalgraawi@gmail.com>
2026-07-25 20:00:01 +0300

    k

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa."""
+"""Module that lists all states with a name matching a user-provided
+argument from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    """Connect to a MySQL server and print states matching sys.argv[4]."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-def main(user, password, database, searched):
-    """Connect to MySQL and print states whose name matches the searche word"""
-    mydb = MySQLdb.connect(host='localhost', port=3306, user=user,
-                           passwd=password, db=database, charset="utf8")
-    mycur = mydb.cursor()
-    mycur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
-    ORDER BY states.id ASC".format(searched))
-    rows = mycur.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password,
+                         db=db_name, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
+ORDER BY states.id ASC".format(state_name))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    mycur.close()
-    mydb.close()
-
-
-if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    cur.close()
+    db.close()
