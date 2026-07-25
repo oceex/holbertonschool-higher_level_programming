57e63698f1203cb63b91174add014e0baa911f63
leen <leenalgraawi@gmail.com>
2026-07-25 20:03:11 +0300

    k

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""Module that lists all states with a name matching a user-provided
-argument from the database hbtn_0e_0_usa.
 """
-import MySQLdb
+Module that lists all states from a MySQL database where the name
+matches the argument passed to the script.
+"""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """Connect to a MySQL server and print states matching sys.argv[4]."""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password,
-                         db=db_name, charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
-ORDER BY states.id ASC".format(state_name))
-    rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cursor = db.cursor()
+    query = (
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' "
+        "ORDER BY states.id ASC"
+    ).format(sys.argv[4])
+    cursor.execute(query)
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cur.close()
+    cursor.close()
     db.close()
