b2c389934e5137d5a825bb2fe2b062fa46ed4eab
leen <leenalgraawi@gmail.com>
2026-07-25 21:34:46 +0300

    k

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa."""
+import MySQLdb
+import sys
+
+
+def main(user, password, database):
+    """Connect to MySQL and lists all cities."""
+    mydb = MySQLdb.connect(host='localhost', port=3306, user=user,
+                           passwd=password, db=database)
+    mycur = mydb.cursor()
+    mycur.execute("SELECT * FROM cities")
+
+    for row in mycur:
+        print(row)
+    mycur.close()
+    mydb.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
