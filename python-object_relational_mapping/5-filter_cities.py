5fc9506ae01f6f6f47b391605e4d7e96185f758d
leen <leenalgraawi@gmail.com>
2026-07-25 22:00:57 +0300

    k

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 """Lists all cities of a given state from the database hbtn_0e_4_usa."""
-import sys
 import MySQLdb
+import sys
 
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-def filter_cities(username, password, database, state_name):
-    """Print every city belonging to state_name, sorted by cities.id."""
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = connection.cursor()
-    cursor.execute(
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=mysql_username, passwd=mysql_password,
+                         db=database_name, charset="utf8")
+
+    cur = db.cursor()
+    cur.execute(
         "SELECT cities.name FROM cities "
         "INNER JOIN states ON cities.state_id = states.id "
         "WHERE states.name = %s "
         "ORDER BY cities.id ASC",
         (state_name,)
     )
-    cities = [row[0] for row in cursor.fetchall()]
+    rows = cur.fetchall()
+    cities = [row[0] for row in rows]
     print(", ".join(cities))
 
-    cursor.close()
-    connection.close()
-
-
-if __name__ == "__main__":
-    filter_cities(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    cur.close()
+    db.close()
