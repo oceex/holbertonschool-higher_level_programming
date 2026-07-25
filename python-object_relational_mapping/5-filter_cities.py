7f953ed8df6e3e0829a3a53aa3f435bb2c482c4f
leen <leenalgraawi@gmail.com>
2026-07-25 23:02:16 +0300

    k

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,45 +1,23 @@
 #!/usr/bin/python3
-"""
-Lists all cities of a given state from the database hbtn_0e_4_usa.
-Usage: ./5-filter_cities.py <mysql_username> <mysql_password>
- <db_name> <state_name>
-Uses MySQLdb; SQL injection safe; results sorted by cities.id.
-"""
-
+"""Script that lists all cities of a state from the database hbtn_0e_4_usa."""
 import MySQLdb
 import sys
 
 
-def main():
-    """Connect to the DB, fetch cities for the given state,
-     and print them comma separated."""
-    if len(sys.argv) != 5:
-        return
-
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    dbname = sys.argv[3]
-    state_name = sys.argv[4]
-
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=user, passwd=passwd, db=dbname,
-                           charset="utf8")
-    cur = conn.cursor()
-
-    query = ("SELECT cities.name "
-             "FROM cities "
-             "JOIN states ON cities.state_id = states.id "
-             "WHERE states.name = %s "
-             "ORDER BY cities.id ASC")
-    cur.execute(query, (state_name,))
-    rows = cur.fetchall()
-
-    names = [r[0] for r in rows]
-    print(", ".join(names))
-
-    cur.close()
-    conn.close()
+def main(user, password, database, state):
+    """Connect to MySQL and print all cities belonging to the given state."""
+    mydb = MySQLdb.connect(host='localhost', port=3306, user=user,
+                           passwd=password, db=database)
+    mycur = mydb.cursor()
+    mycur.execute("SELECT cities.name FROM cities "
+                  "INNER JOIN states ON cities.state_id = states.id "
+                  "WHERE states.name = %s "
+                  "ORDER BY cities.id ASC", (state,))
+    cities = [row[0] for row in mycur]
+    print(", ".join(cities))
+    mycur.close()
+    mydb.close()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
