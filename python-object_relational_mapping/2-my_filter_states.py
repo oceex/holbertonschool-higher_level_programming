bd37b8d47c761f1a63f335e06d7e05dbe661164f
leen <leenalgraawi@gmail.com>
2026-07-25 16:21:01 +0300

    k

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name matching the argument"""
+"""Script that lists all states from the database hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
-    cursor = db.cursor()
+def main(user, password, database, searched):
+    """Connect to MySQL and print states whose name matches the searche word"""
+    mydb = MySQLdb.connect(host='localhost', port=3306, user=user,
+                           passwd=password, db=database, charset="utf8")
+    mycur = mydb.cursor()
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name)
-    cursor.execute(query)
-
-    for row in cursor.fetchall():
+        searched)
+    mycur.execute(query)
+    for row in mycur.fetchall():
         print(row)
 
-    cursor.close()
-    db.close()
+    mycur.close()
+    mydb.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
