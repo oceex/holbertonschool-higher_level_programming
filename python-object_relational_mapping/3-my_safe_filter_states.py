55a6bd6bcbc1a47d96ecfbb05dfc5535eaeadd82
leen <leenalgraawi@gmail.com>
2026-07-27 02:40:19 +0300

    k

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""Script that lists all states matching a name from the database
+"""
+Script that lists all states matching a name from the database
 hbtn_0e_0_usa, safe from MySQL injection.
 """
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    """Connect to MySQL server and display states matching the given name.
 
-    Takes 4 arguments: mysql username, mysql password, database name
-    and the state name to search for. The query is parameterized to
-    protect against SQL injection.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
+def main(usr, pas, db, arg):
+    """" takes in arguments and displays all values in the states table """
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password,
-                         db=db_name, charset="utf8")
+                         user=usr, passwd=pas,
+                         db=db, charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s "
-                "ORDER BY id ASC", (state_name,))
+    cur.execute("SELECT * FROM states WHERE name = %s "
+                "ORDER BY id ASC", (arg,))
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
