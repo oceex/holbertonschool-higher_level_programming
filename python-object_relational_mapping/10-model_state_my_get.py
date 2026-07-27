4018c838b78577347d94c8ed2fff67979b61dafa
leen <leenalgraawi@gmail.com>
2026-07-27 03:01:49 +0300

    l

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument,
-searched safely from the database hbtn_0e_6_usa."""
+"""Prints the State object with the name passed as argument (argv[4]),
+searched safely from argv[3]."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
+
+def main(usr, pas, db, arg):
+    """"  prints the State object with the name passed as argument from db """
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(usr, pas, db),
         pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    state = session.query(State).filter(State.name == arg).first()
 
-    if state is None:
-        print("Not found")
-    else:
+    if state:
         print(state.id)
+    else:
+        print("Not found")
 
     session.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
