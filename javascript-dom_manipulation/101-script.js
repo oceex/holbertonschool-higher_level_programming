9e7cea2fd7fd2f316f4e1740b000ae2b9410f52f
leen <leenalgraawi@gmail.com>
2026-08-10 01:34:10 +0300

    k

--- javascript-dom_manipulation/101-script.js
+++ javascript-dom_manipulation/101-script.js
@@ -1,4 +1,18 @@
 #!/usr/bin/node
 document.addEventListener('DOMContentLoaded', () => {
+  const translateButton = document.getElementById('btn_translate');
+  const languageSelect = document.getElementById('language_code');
+  const helloElement = document.getElementById('hello');
 
+  translateButton.addEventListener('click', () => {
+    const languageCode = languageSelect.value;
+    fetch(`https://hellosalut.stefanbohacek.com/?lang=${languageCode}`)
+      .then((response) => response.json())
+      .then((data) => {
+        helloElement.innerHTML = data.hello;
+      })
+      .catch((error) => {
+        console.error('Error fetching the translation:', error);
+      });
+  });
 });
