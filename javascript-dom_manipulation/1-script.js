d8040644ea1434ffd7c9636ab4105ec56019f22f
leen <leenalgraawi@gmail.com>
2026-08-09 03:48:51 +0300

    k

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const headers = document.querySelectorAll('header');
+const div = document.querySelector('#red_header');
+div.addEventListener('click', e => { headers.forEach(header => { header.style.color = 'red'; }); });
